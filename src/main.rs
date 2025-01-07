0dd78fc108f01ad0bcd325d79716953cf0a7424a
Jose Manuel Hidalgo López <jose.hidalgo@stalicla.com>
2025-01-07 16:33:26 +0100

    feat: improved file open/closing menus
    Selected files are now stored within a Cursive user vector

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,28 @@ use std::path::Path;
 
 fn main() {
     let mut siv = cursive::default();
+    siv.set_user_data(Vec::<String>::new());
     let cwd = ".";
     siv.menubar()
         .add_subtree(
             "File",
             menu::Tree::new()
                 .leaf("Open...", move |s| {
-                    s.add_layer(create_dir_box(cwd));
+                    let file_list = s.take_user_data::<Vec<String>>().unwrap_or_default();
+                    let (new_layer, updated_file_list) = create_dir_box(cwd, file_list);
+                    s.set_user_data(updated_file_list);
+                    s.add_layer(new_layer);
                 })
-                .leaf("Close...", |s| s.add_layer(Dialog::info("Close file!"))),
+                .leaf("Close...", move |s: &mut cursive::Cursive| {
+                    let file_list = s.take_user_data::<Vec<String>>().unwrap_or_default();
+                    if file_list.is_empty() {
+                        s.add_layer(Dialog::info("No file opened"));
+                    } else {
+                        let (new_layer, updated_file_list) = close_box(file_list);
+                        s.set_user_data(updated_file_list);
+                        s.add_layer(new_layer);
+                    }
+                }),
         )
         .add_subtree(
             "Filter",
@@ -42,26 +55,57 @@ fn main() {
     siv.run();
 }
 
-fn create_dir_box(initial_path: &str) -> Dialog {
+fn close_box(file_list: Vec<String>) -> (Dialog, Vec<String>) {
+    let mut select_view = SelectView::new().h_align(HAlign::Center);
+    let new_file_list: Vec<String> = file_list.clone();
+    select_view.add_all_str(file_list);
+    select_view.set_on_submit(move |s, selected: &str| {
+        let mut new_file_list = s.take_user_data::<Vec<String>>().unwrap_or_default();
+        new_file_list.retain(|element| element != selected);
+        s.pop_layer();
+        s.set_user_data(new_file_list);
+    });
+    (
+        Dialog::around(select_view)
+            .title("Close file:")
+            .button("Cancel", |s| {
+                s.pop_layer();
+            }),
+        new_file_list,
+    )
+}
+
+fn create_dir_box(initial_path: &str, file_list: Vec<String>) -> (Dialog, Vec<String>) {
     let path = initial_path.to_string();
     let mut select_view = SelectView::new().h_align(HAlign::Center);
+    let new_file_list = file_list;
     select_view.add_all_str(list_dir(initial_path));
+
     select_view.set_on_submit(move |s, selected: &str| {
+        let mut new_file_list = s.take_user_data::<Vec<String>>().unwrap_or_default();
+
         if selected == ".." {
             if let Some(parent) =
                 Path::new(&std::path::absolute(&path).expect("ERROR: Could not get Path!")).parent()
             {
                 let new_path = parent.to_str().unwrap();
                 s.pop_layer();
-                s.add_layer(create_dir_box(new_path));
+                let (new_layer, updated_file_list) = create_dir_box(new_path, new_file_list);
+                s.set_user_data(updated_file_list);
+                s.add_layer(new_layer);
             }
         } else {
             let selected_path = Path::new(&path).join(selected);
             if selected_path.is_dir() {
                 s.pop_layer();
-                s.add_layer(create_dir_box(selected_path.to_str().unwrap()));
+                let (new_layer, updated_file_list) =
+                    create_dir_box(selected_path.to_str().unwrap(), new_file_list);
+                s.set_user_data(updated_file_list);
+                s.add_layer(new_layer);
             } else {
                 let text = format!("You opened file: {}", selected_path.display());
+                new_file_list.push(selected_path.to_str().unwrap().to_string());
+                s.set_user_data(new_file_list);
                 s.add_layer(Dialog::around(TextView::new(text)).button("OK", |s| {
                     s.pop_layer();
                     s.pop_layer();
@@ -69,11 +113,15 @@ fn create_dir_box(initial_path: &str) -> Dialog {
             }
         }
     });
-    Dialog::around(select_view)
-        .title(format!("Browsing: {}", initial_path))
-        .button("Cancel", |s| {
-            s.pop_layer();
-        })
+
+    (
+        Dialog::around(select_view)
+            .title(format!("Browsing: {}", initial_path))
+            .button("Cancel", |s| {
+                s.pop_layer();
+            }),
+        new_file_list,
+    )
 }
 
 fn list_dir(path_str: &str) -> Vec<String> {
