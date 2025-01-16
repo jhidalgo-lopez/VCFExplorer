3ed42b880774f00b23b2efce2840566c0a44194b
Jose Manuel Hidalgo López <jose.hidalgo@stalicla.com>
2025-01-16 21:56:54 +0100

    feat(main.rs): reads vcfs an displays as table

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use cursive::align::HAlign;
+use cursive::traits::Nameable;
 use cursive::CursiveRunnable;
 use cursive::{
     event::Key,
     menu,
     traits::*,
-    views::{Dialog, SelectView, TextView},
+    views::{Dialog, LinearLayout, ScrollView, SelectView, TextView},
 };
-use rust_htslib::bcf::Reader;
+use rust_htslib::bcf::{Read, Reader};
 use std::path::Path;
+
 fn main() {
     let mut siv = add_ui();
     siv.run();
 }
 
+// UI Section
 fn close_box(file_list: Vec<String>) -> (Dialog, Vec<String>) {
     let mut select_view = SelectView::new().h_align(HAlign::Center);
     let new_file_list: Vec<String> = file_list.clone();
@@ -50,7 +53,7 @@ fn create_dir_box(initial_path: &str, file_list: Vec<String>) -> (Dialog, Vec<St
                 s.pop_layer();
                 let (new_layer, updated_file_list) = create_dir_box(new_path, new_file_list);
                 s.set_user_data(updated_file_list);
-                s.add_layer(new_layer);
+                s.add_layer(new_layer.with_name("open_file_box"));
             }
         } else {
             let selected_path = Path::new(&path).join(selected);
@@ -59,15 +62,24 @@ fn create_dir_box(initial_path: &str, file_list: Vec<String>) -> (Dialog, Vec<St
                 let (new_layer, updated_file_list) =
                     create_dir_box(selected_path.to_str().unwrap(), new_file_list);
                 s.set_user_data(updated_file_list);
-                s.add_layer(new_layer);
+                s.add_layer(new_layer.with_name("open_file_box"));
             } else {
-                read_vcf(selected_path.to_str().unwrap());
                 let text = format!("You opened file: {}", selected_path.display());
                 new_file_list.push(selected_path.to_str().unwrap().into());
                 s.set_user_data(new_file_list);
                 s.add_layer(Dialog::around(TextView::new(text)).button("OK", |s| {
+                    let new_file_list = s.take_user_data::<Vec<String>>().unwrap_or_default();
                     s.pop_layer();
                     s.pop_layer();
+                    // s.pop_layer();
+                    let table_view = update_table(&new_file_list);
+                    s.set_user_data(new_file_list);
+                    s.add_layer(
+                        Dialog::around(ScrollView::new(table_view))
+                            .title("VCF Viewer")
+                            .full_screen()
+                            .with_name("vcf_data"),
+                    );
                 }));
             }
         }
@@ -82,7 +94,15 @@ fn create_dir_box(initial_path: &str, file_list: Vec<String>) -> (Dialog, Vec<St
         new_file_list,
     )
 }
-
+fn update_table(file_list: &Vec<String>) -> LinearLayout {
+    // Table view
+    let mut vcf_data_all: Vec<VcfRecord> = Vec::new();
+    for file in file_list {
+        vcf_data_all.append(&mut read_vcf(&file));
+    }
+    let table_view = create_table(vcf_data_all);
+    table_view
+}
 fn list_dir(path_str: &str) -> Vec<String> {
     let path = Path::new(path_str);
     let mut entries = vec!["..".to_string()];
@@ -103,7 +123,15 @@ fn list_dir(path_str: &str) -> Vec<String> {
 
 fn add_ui() -> CursiveRunnable {
     let mut siv = cursive::default();
+    siv.set_autorefresh(true);
     siv.set_user_data(Vec::<String>::new());
+    // Table view
+    let table_view = create_table(Vec::new());
+    siv.add_layer(
+        Dialog::around(ScrollView::new(table_view))
+            .title("VCF Viewer")
+            .full_screen(),
+    );
     let cwd = ".";
     siv.menubar()
         .add_subtree(
@@ -148,6 +176,113 @@ fn add_ui() -> CursiveRunnable {
     siv
 }
 
-fn read_vcf(path_str: &str) {
-    let bcf = Reader::from_path(path_str).expect("Error opening file: {path_str:?}.");
+fn create_table(records: Vec<VcfRecord>) -> LinearLayout {
+    let mut layout = LinearLayout::vertical();
+    // Add the header
+    layout.add_child(
+        LinearLayout::horizontal()
+            .child(
+                TextView::new("Chromosome")
+                    .h_align(HAlign::Center)
+                    .fixed_width(15),
+            )
+            .child(
+                TextView::new("Position")
+                    .h_align(HAlign::Center)
+                    .fixed_width(10),
+            )
+            .child(TextView::new("ID").h_align(HAlign::Center).fixed_width(20))
+            .child(
+                TextView::new("Ref Allele")
+                    .h_align(HAlign::Center)
+                    .fixed_width(10),
+            )
+            .child(
+                TextView::new("Alt Allele")
+                    .h_align(HAlign::Center)
+                    .fixed_width(10),
+            ),
+    );
+
+    layout.add_child(TextView::new("-".repeat(70)));
+
+    // Add records to the table
+    for record in records {
+        layout.add_child(
+            LinearLayout::horizontal()
+                .child(TextView::new(record.chromosome).min_width(10))
+                .child(TextView::new(record.position.to_string()).min_width(15))
+                .child(TextView::new(record.id.to_string()).min_width(10))
+                .child(TextView::new(record.ref_allele).min_width(30))
+                .child(TextView::new(record.alt_allele).min_width(30)),
+        );
+    }
+
+    layout
+}
+
+// VCF SECTION
+// fn read_vcf(path_str: &str) {
+//     let mut bcf = Reader::from_path(path_str).expect("Error opening file: {path_str:?}.");
+//     for (_, record_result) in bcf.records().enumerate() {
+//         let record = record_result.expect("Failed to read record");
+//         let mut s = String::new();
+//         for allele in record.alleles() {
+//             for base in allele {
+//                 s.push(char::from(*base))
+//             }
+//         }
+//         // println!("Locus: {}, Alleles: {}", record.pos(), s)
+//     }
+// }
+// Struct for storing VCF record information
+#[derive(Debug)]
+struct VcfRecord {
+    chromosome: String,
+    position: i64,
+    id: u32,
+    ref_allele: String,
+    alt_allele: String,
+}
+
+// Function to read VCF data
+fn read_vcf(path_str: &str) -> Vec<VcfRecord> {
+    let mut records = Vec::new();
+    // let path = Path::new(path_str);
+    // if !path.exists() {
+    //     println!("VCF file not found: {}", path_str);
+    //     return records;
+    // }
+    let mut bcf = Reader::from_path(path_str).expect("Error opening file: {path_str:?}.");
+    for result in bcf.records() {
+        // let result = result.expect("Failed to read record");
+        if let Ok(record) = result {
+            let mut chromosome = String::new();
+            if let Ok(chr) = record.header().rid2name(record.rid().unwrap()) {
+                for c in chr {
+                    chromosome.push(*c as char);
+                }
+            }
+
+            let position = record.pos();
+            let id = record.rid().unwrap();
+            let mut ref_allele = String::new();
+            for allele in record.alleles()[0] {
+                ref_allele.push(char::from(*allele))
+            }
+            let mut alt_allele = String::new();
+            for allele in record.alleles()[1] {
+                alt_allele.push(char::from(*allele))
+            }
+            let entry = VcfRecord {
+                chromosome,
+                position,
+                id,
+                ref_allele,
+                alt_allele,
+            };
+            records.push(entry);
+        }
+    }
+    records
 }
