ba9b3ada771552b88284a3214c4baac60914f1e0
Jose Manuel Hidalgo-Lopez <53059588+jhidalgo-lopez@users.noreply.github.com>
2025-10-10 15:15:24 +0000

    feat(main.rs): adds logging tool

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod tui;
 mod vcf;
+use simplelog::*;
+use std::fs::File;
 
 fn main() {
+    CombinedLogger::init(vec![WriteLogger::new(
+        LevelFilter::Info,
+        Config::default(),
+        File::create("vcf_explorer.log").unwrap(),
+    )])
+    .unwrap();
+
     let mut siv = tui::add_ui();
     siv.run();
 }
